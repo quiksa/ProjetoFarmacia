7bc241835fad0bf695de1f701707d48a4f5a7551
Guilherme Seibert <quiksa@hotmail.com>
2018-08-09 23:54:28 -0300

    Arquivos do Projeto

---
+++ src/test/java/com/unisc/ProjetoFarmacia/ProjetoFarmaciaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unisc.ProjetoFarmacia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoFarmaciaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
