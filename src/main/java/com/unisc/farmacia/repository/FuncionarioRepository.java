048558fc4643fd8ed5ca2cf17b94d14f1b79b49e
Douglas Alves <alves_543@hotmail.com>
2018-09-13 14:08:46 -0300

    Tentando retornar consulta de usuario e senha

--- src/main/java/com/unisc/farmacia/repository/FuncionarioRepository.java
+++ src/main/java/com/unisc/farmacia/repository/FuncionarioRepository.java
@@ -1,10 +1,16 @@
 package com.unisc.farmacia.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.unisc.farmacia.model.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+	
+	@Query(value="select * from funcionario where login =?1 and senha=?2",nativeQuery = true)
+	Optional<Funcionario>listaFuncionario(String login, String senha);
 
 	
 }
--- src/main/java/com/unisc/farmacia/resources/FuncionarioResources.java
+++ src/main/java/com/unisc/farmacia/resources/FuncionarioResources.java
@@ -1,11 +1,14 @@
 package com.unisc.farmacia.resources;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,34 +16,45 @@ import com.unisc.farmacia.model.Funcionario;
 import com.unisc.farmacia.repository.FuncionarioRepository;
 
 @RestController
-@RequestMapping("/funcionario")
 public class FuncionarioResources {
 
 	@Autowired
 	private FuncionarioRepository fr;
-	
-	@GetMapping(produces="application/json")
+
+	@GetMapping("/funcionario")
 	public @ResponseBody Iterable<Funcionario> listaFuncionarios() {
 		Iterable<Funcionario> listaFuncionarios = fr.findAll();
-		
+
 		return listaFuncionarios;
 	}
-	/*
-	public @ResponseBody Optional<Funcionario> exibeFuncionario(int id){
-		Optional<Funcionario> funcionario = fr.findById(id);
-		return funcionario;
-	}
-	*/
-	
-	@PostMapping()
+
+	@PostMapping("/funcionario")
 	public Funcionario insereFuncionario(@RequestBody Funcionario funcionario) {
 		return fr.save(funcionario);
 	}
-	
-	@DeleteMapping
+
+	@DeleteMapping("/funcionario")
 	public Funcionario deletaFuncionario(@RequestBody Funcionario funcionario) {
 		fr.delete(funcionario);
 		return funcionario;
 	}
+
+	// Por item único
+
+	@GetMapping("/funcionario/{id}")
+	public @ResponseBody Optional<Funcionario> retornaFuncionarioPorId(@PathVariable Integer id) {
+		Optional<Funcionario> funcionario = fr.findById(id);
+		return funcionario;
+	}
 	
+	@GetMapping("/funcionario/{login}/{senha}")
+	public @ResponseBody Optional<Funcionario>retornaFuncionario(@RequestParam String login, @RequestParam String senha){
+		
+		Optional<Funcionario>funcionario = fr.listaFuncionario(login,senha);
+		return funcionario;
+		
+	}
+	
+	
+
 }
