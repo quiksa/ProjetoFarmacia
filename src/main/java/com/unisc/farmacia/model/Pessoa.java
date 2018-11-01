0fe6e8cdfecbc2b177cf595ff8abf1ba4ac443f6
Guilherme Seibert <quiksa@hotmail.com>
2018-11-01 20:49:11 -0300

    add novos atributis

--- src/main/java/com/unisc/farmacia/model/Pessoa.java
+++ src/main/java/com/unisc/farmacia/model/Pessoa.java
@@ -1,5 +1,7 @@
 package com.unisc.farmacia.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,58 +10,100 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
 @Entity
-@Table(name="pessoa")
+@Table(name = "pessoa")
 public class Pessoa {
 
 	@Id
-	@Column(name="idpessoa")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "idpessoa")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idPessoa;
-	
-	@Column (name="nmpessoa")
+
+	@Column(name = "nmpessoa")
 	private String nmPessoa;
-	
+
+	@Column(name = "sgsexo")
+	private String sgsexo;
+
+	@Column(name = "email")
+	private String email;
+
 	@ManyToOne
-	@JoinColumn(name="idendereco")
+	@JoinColumn(name = "idendereco")
 	private Endereco endereco;
-	
+
 	public String getNrcpf() {
 		return nrcpf;
 	}
+
 	public void setNrcpf(String nrcpf) {
 		this.nrcpf = nrcpf;
 	}
+
 	public String getNrtelefone() {
 		return nrtelefone;
 	}
+
 	public void setNrtelefone(String nrtelefone) {
 		this.nrtelefone = nrtelefone;
 	}
-	@Column(name="nrcpf")
-	private String  nrcpf;
-	
-	@Column(name="nrtelefone")
+
+	@Column(name = "nrcpf")
+	private String nrcpf;
+
+	@Column(name = "nrtelefone")
 	private String nrtelefone;
-	
-	
+
+	@Column(name = "dtnascimento")
+	private LocalDate dtnascimento;
+
 	public Endereco getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
-		
+
 	public int getIdPessoa() {
 		return idPessoa;
 	}
+
 	public void setIdPessoa(int idPessoa) {
 		this.idPessoa = idPessoa;
 	}
+
 	public String getNmPessoa() {
 		return nmPessoa;
 	}
+
 	public void setNmPessoa(String nmPessoa) {
 		this.nmPessoa = nmPessoa;
 	}
+
+	public LocalDate getDtnascimento() {
+		return dtnascimento;
+	}
+
+	public void setDtnascimento(LocalDate dtnascimento) {
+		this.dtnascimento = dtnascimento;
+	}
+
+	public String getSgsexo() {
+		return sgsexo;
+	}
+
+	public void setSgsexo(String sgsexo) {
+		this.sgsexo = sgsexo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
