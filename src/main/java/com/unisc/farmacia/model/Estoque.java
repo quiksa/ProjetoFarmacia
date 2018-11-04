ba28565bde07985f2f3a4df7b9cbf1e5e6ca46a0
Douglas Alves <alves_543@hotmail.com>
2018-11-04 02:01:16 -0200

    Insert/Update de Estoque

--- src/main/java/com/unisc/farmacia/model/Estoque.java
+++ src/main/java/com/unisc/farmacia/model/Estoque.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name="estoque")
@@ -21,6 +22,63 @@ public class Estoque {
 	@OneToOne
 	@JoinColumn(name="idunidade")
 	private Unidade unidade;
+	
+	
+	/**************************
+	 * TRANSIENTS DE UNIDADE***
+	 *************************/
+	
+	@Transient
+	private String dsUnidade;
+	
+	@Transient
+	private String nmUnidade;
+	
+	@Transient
+	private String nmReduzido;
+	
+	@Transient
+	private String cnpj;
+	
+	@Transient
+	private String idEndereco;
+	
+	
+	
+	
+	public String getDsUnidade() {
+		return dsUnidade;
+	}
+	public void setDsUnidade(String dsUnidade) {
+		this.dsUnidade = dsUnidade;
+	}
+	public String getNmUnidade() {
+		return nmUnidade;
+	}
+	public void setNmUnidade(String nmUnidade) {
+		this.nmUnidade = nmUnidade;
+	}
+	public String getNmReduzido() {
+		return nmReduzido;
+	}
+	public void setNmReduzido(String nmReduzido) {
+		this.nmReduzido = nmReduzido;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getIdEndereco() {
+		return idEndereco;
+	}
+	public void setIdEndereco(String idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+	
+	//-----------------------------------------------------//
+	
 	
 	public int getIdEstoque() {
 		return idEstoque;
--- src/main/java/com/unisc/farmacia/resources/EstoqueResources.java
+++ src/main/java/com/unisc/farmacia/resources/EstoqueResources.java
@@ -1,16 +1,27 @@
 package com.unisc.farmacia.resources;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unisc.farmacia.model.Cidade;
+import com.unisc.farmacia.model.Endereco;
 import com.unisc.farmacia.model.Estoque;
+import com.unisc.farmacia.model.Unidade;
+import com.unisc.farmacia.repository.EnderecoRepository;
 import com.unisc.farmacia.repository.EstoqueRepository;
+import com.unisc.farmacia.repository.UnidadeRepository;
 
 @RestController
 @RequestMapping("/estoque")
@@ -18,6 +29,10 @@ public class EstoqueResources {
 
 	@Autowired
 	private EstoqueRepository er;
+	@Autowired
+	private EnderecoRepository ery;
+	@Autowired
+	private UnidadeRepository ur;
 	
 	@GetMapping(produces="application/json")
 	public @ResponseBody Iterable<Estoque> listaEstoques() {
@@ -25,12 +40,6 @@ public class EstoqueResources {
 		
 		return listaEstoques;
 	}
-	/*
-	public @ResponseBody Optional<Estoque> exibeEstoque(int id){
-		Optional<Estoque> estoque = er.findById(id);
-		return estoque;
-	}
-	*/
 	
 	@PostMapping()
 	public Estoque insereEstoque(@RequestBody Estoque estoque) {
@@ -43,4 +52,35 @@ public class EstoqueResources {
 		return estoque;
 	}
 	
+	
+	@Transactional
+	@RequestMapping(value = "/insertOrUpdadeEstoque", method = RequestMethod.POST, consumes = "application/json")
+	public ResponseEntity<Estoque> retornaEstoque(@RequestBody Estoque estoque) {
+		try {
+			Estoque est = new Estoque();
+			if (!estoque.getCnpj().equals("") && !estoque.getDsUnidade().equals("") && !estoque.getNmReduzido().equals("") &&
+				!estoque.getNmUnidade().equals("") && !estoque.getCnpj().equals("") && !estoque.getIdEndereco().equals("") &&
+				!estoque.getIdEndereco().equals("")) {
+				Optional<Endereco> en = ery.findById(Integer.parseInt(estoque.getIdEndereco()));
+				Unidade un = new Unidade();
+				un.setCnpj(estoque.getCnpj());
+				un.setDsUnidade(estoque.getDsUnidade());
+				un.setNmRduzido(estoque.getNmReduzido());
+				un.setNmUnidade(estoque.getNmUnidade());
+				if(estoque.getIdEndereco()!=null) {
+					un.setIdendereco(estoque.getIdEndereco());
+				}
+				ur.save(un);
+				ur.flush();
+				est.setUnidade(un);
+				er.save(est);
+				er.flush();
+			} else {
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
+			return new ResponseEntity<>(est, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
 }
